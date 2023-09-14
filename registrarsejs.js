3a36a956d9a30997ad5681893ce047bed8b4b2d8
galRubel <127147440+galRubel@users.noreply.github.com>
2023-09-14 08:21:27 -0300

    fix: Mejore la seccion divider de la lanidng

--- registrarsejs.js
+++ registrarsejs.js
@@ -1,18 +1,29 @@
-const loginForm = document.getElementById('loginForm');
-
-
+document.addEventListener("DOMContentLoaded", (event) => {
+  const loginForm = document.getElementById('loginForm');
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
   const confirmpasswo = document.getElementById('confrimpassword').value;
-  const boton = document.getElementById("button-crear");
+
+  loginForm.addEventListener("submit", (event) => {
+    console.log("Pssword: " + password)
+    event.preventDefault();
+    if (password === confirmpasswo && password != "") {
+      alert(password + " " + confirmpasswo);
+    }
+    else {
+      alert("Usuario refistrado ")
+    }
+  })
+});
 
 
+/*
 boton.onclick = function() {
-  if(password != confirmpasswo) {
-    alert("No seas potz");
-  } else{
-    alert("Usuario registrado")
+  if(password === confirmpasswo && password != "") {
+    alert(password + " " + confirmpasswo);
   }
-} 
-   
-;
+  else{
+    alert("Usuario refistrado ")
+  }
+}
+   */
