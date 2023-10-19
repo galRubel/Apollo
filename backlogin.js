f886b800dd5af55de704727fbe3ed6d8e3956b15
galRubel <127147440+galRubel@users.noreply.github.com>
2023-10-19 15:55:26 -0300

    fix: podes mandar requests en express

--- backlogin.js
+++ backlogin.js
@@ -1,20 +1,37 @@
 // Import the required modules
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 const express = require('express');
 const app = express();
+const cors = require("cors")
+const port = process.env.PORT || 3000;
+app.use(express.json());
 
-const port = 3000; 
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 //AJAX
 app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello, API!' });
-  });
-app.post("/auth/login", async (req, res) => {
-  console.log(req.body)
-  res.json({message: "HOLIS"})
+  res.json({ message: 'Hello, API!' });
+});
+
+app.get("/gal", (req, res) => {
+  res.send("Hello Gal")
 })
-  
+
+app.post("/registrarse.html", (req, res) => {
+  const username = req.username;
+  const pwd = req.password;
+  const user = {
+    "name": username,
+    "password": pwd,
+  }
+  console.log(req.body)
+  res.send(user)
+})
+
+
 // Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:3000`);
--- registrarsejs.js
+++ registrarsejs.js
@@ -4,14 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
   var crear = document.getElementById('button-crear');
   var message = document.getElementById('message');
 
-  crear.addEventListener('click', function (event) {
+  crear.addEventListener('click',async function (event) {
     event.preventDefault(); // Prevent the form from submitting normally
-    const res = await fetch("http://localhost/algo.php", {
+    const res = await fetch("http://localhost:3000/registrarse.html", {
       method: "POST",
       body: JSON.stringify({
         username: username,
         password: password,
-      })
-    })
-   
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((res)=>{
+      const data = res.json();
+      console.log(data) 
+    })  
   })})
