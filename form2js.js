9d3c7623da9335a2abdb399e84430f4abaea637c
galRubel <127147440+galRubel@users.noreply.github.com>
2023-11-07 12:06:52 -0300

    feat:conexion entre forms

--- form2js.js
+++ form2js.js
@@ -1,20 +1,5 @@
 const inputField = document.getElementById("dia");
-const input = document.getElementById("mes");
+const peso = document.getElementById("peso");
+const altura = document.getElementById("altura");
 inputField.addEventListener("input", function() {
-    const currentCount = inputField.value.length;
-    const maxLength = 2
-    
-    
-    
-    // Disable input when the character limit is reached (optional)
-    if (currentCount >= maxLength) {
-        inputField.value = inputField.value.slice(0, maxLength);
-    } else {
-        inputField.disabled = false;
-    }
-    if (input >= maxLength) {
-        inputField.value = input.value.slice(0, maxLength);
-    } else {
-        input.disabled = false;
-    }
 });
